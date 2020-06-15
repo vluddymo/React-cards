a96798182431567c5d39df1e23bb8e85e80a3508
Wladi Moser <wladi.moser@gmail.com>
2020-06-15 16:19:27 +0200

    Add Card component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import MainContent from "./components/MainContent/MainContent";
+import Card from "./components/Card/Card";
 
 function App() {
   return (
     <div className="App">
       <Header>Website Title</Header>
-      <MainContent>main</MainContent>
+      <MainContent>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+      </MainContent>
       <Footer>Footer text</Footer>
     </div>
   );
---
+++ src/components/Card/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './Card.css';
+
+export default function Card(){
+  return (
+      <article>
+        <section>
+          <h2></h2>
+          <p></p>
+        </section>
+        <section>
+          <img src="" alt=""/>
+        </section>
+      </article>
+  )
+}
